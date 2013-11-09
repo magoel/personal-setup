7fc2e5a4bbbfb1fa1b93d4f863a3d841e521a166
Manish Goel <manish.dce@gmail.com>
2013-11-09 16:38:51 +0000

    added preference for vim

--- core-setup.sh
+++ core-setup.sh
@@ -6,3 +6,33 @@
 # https://github.com/creationix/nvm
 sudo apt-get install -y git
 sudo apt-get install -y curl
+
+
+
+#install vim-pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+touch ~/.vimrc
+echo 'execute pathogen#infect()' >> ~/.vimrc
+
+
+#install vim-sensible
+pushd ~/.vim/bundle
+git clone git://github.com/tpope/vim-sensible.git
+popd
+#configure vim-sensible to be loaded before .vimrc , so that one can over-ride in .vimrc
+touch ~/.vimrc
+echo 'runtime! plugin/sensible.vim' >> ~/.vimrc
+echo '#Recommendation : Write any prefernce setting below this line only' >> ~/.vimrc
+#intsall vim-slime
+pushd ~/.vim/bundle
+git clone git://github.com/jpalardy/vim-slime.git
+popd
+#configure slime for screen
+touch ~/.vimrc
+echo 'let g:slime_target = "screen"' >> ~/.vimrc
+
+
+
+
--- setup.csh
+++ setup.csh
@@ -3,11 +3,13 @@
 set script_dir = `dirname $0`
 pushd $script_dir;
 source core-setup.sh
-# intsall utility scripts
-
-
-
-
 source preference-setup.csh
+popd
+
+# intsall utility scripts
+mkdir -p $HOME/Trash
+mkdir -p $HOME/Scripts
+
+
 
 
